66987f014014cdfe4664eb46c5ccaffd7850a7dd
sotowang <sotowang@qq.com>
2018-10-19 15:17:23 +0800

    写入MySQL

---
+++ src/main/scala/com/soto/log/AccessStat.scala
@@ -0,0 +1,7 @@
+package com.soto.log
+
+/**
+  * 实体类
+  */
+case class AccessStat(item_id:String,cart_id:String,action_type:String,times:Long)
+
--- src/main/scala/com/soto/log/TopNStatJob.scala
+++ src/main/scala/com/soto/log/TopNStatJob.scala
@@ -2,6 +2,8 @@ package com.soto.log
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
+
+import scala.collection.mutable.ListBuffer
 /**
   * TopN统计Spark作业
   */
@@ -11,8 +13,8 @@ object TopNStatJob {
 
     val accessDF = spark.read.format("parquet").load("/home/sotowang/Downloads/datagrand_0517/clean")
 
-    accessDF.printSchema()
-    accessDF.show(false)
+//    accessDF.printSchema()
+//    accessDF.show(false)
 
     videoAccessTopNStat(spark,accessDF)
 
@@ -28,7 +30,7 @@ object TopNStatJob {
       .groupBy("item_id","cart_id","action_type").agg(count("item_id").as("times"))
       .orderBy($"times".desc)
 
-    accessTopNDF.show(false)
+//    accessTopNDF.show(false)
 
 
 //    accessDF.createOrReplaceTempView("access_logs")
@@ -46,6 +48,30 @@ object TopNStatJob {
 //       action_type varchar(20) not null
 //       );
 
+    /**
+      * 将统计结果写到MySQL中
+      */
+    try{
+      accessTopNDF.foreachPartition(partitionOfRecords =>{
+        val list = new ListBuffer[AccessStat]
+
+        partitionOfRecords.foreach(info =>{
+          val item_id = info.getAs[String]("item_id")
+          val cart_id = info.getAs[String]("cart_id")
+          val action_type = info.getAs[String]("action_type")
+          val times = info.getAs[Long]("times")
+
+          list.append(AccessStat(item_id,cart_id,action_type,times))
+        })
+
+        StatDAO.insertAccessTopN(list)
+
+
+      })
+
+    }catch {
+      case e:Exception => e.printStackTrace()
+    }
 
   }
 }
